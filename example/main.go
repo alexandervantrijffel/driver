5b6552decbac3561f10fa20c1bc7ea8723983d6a
Alexander <lexmail@gmail.com>
2019-02-01 00:24:11 +0100

    Test append and watch

--- example/main.go
+++ example/main.go
@@ -12,17 +12,18 @@ import (
 func main() {
 	// create streamsdb connection
 	conn := sdb.MustOpen("sdb://sdb03.streamsdb.io:443?tls=1")
-	db := conn.DB("example")
+	db := conn.DB("logs")
 	defer conn.Close()
 
 	// create a channel to get notified from any errors
 	errs := make(chan error)
 
+	streamName := "test2"
 	// read user input from stdin and append it to the stream
 	go func() {
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
-			_, err := db.Append("inputs", sdb.MessageInput{Value: scanner.Bytes()})
+			_, err := db.Append(streamName, sdb.MessageInput{Value: scanner.Bytes()})
 			if err != nil {
 				errs <- errors.Wrap(err, "append error")
 				return
@@ -32,7 +33,7 @@ func main() {
 
 	// watch the inputs streams for messages and print them
 	go func() {
-		watch := db.Watch("inputs", -1, 10)
+		watch := db.Watch(streamName, -1, 10)
 		for slice := range watch.Slices {
 			for _, msg := range slice.Messages {
 				println("received: ", string(msg.Value))
